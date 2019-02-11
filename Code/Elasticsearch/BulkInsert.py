261610c0547cb44d9d09c01d859c8623f35ae65f
Jordan Myers <jordan@audrey.dsg.cs.tcd.ie>
2019-02-11 20:28:15 +0000

    Updated insert script

--- Code/Elasticsearch/BulkInsert.py
+++ Code/Elasticsearch/BulkInsert.py
@@ -4,6 +4,7 @@
 
 import argparse
 import json
+import time,datetime
 from elasticsearch import Elasticsearch # install via  "$ sudo pip install elasticsearch"
 from elasticsearch.helpers import bulk
 
@@ -29,14 +30,20 @@ if args.es_index is not None:
 es = Elasticsearch()
 
 def genLoadJson():
+    linecount=0
     with open(infile,'r') as f:
         for line in f:
             j_content = json.loads(line)
+	    linecount += 1
+            if (linecount % 100 == 0):
+                print(linecount, " records inserted")
+
             yield {
                 "_index": es_index,
                 "_type": "document",
                 "doc": j_content
             }
 
-
-bulk(es, genLoadJson())
+print(datetime.datetime.utcnow())
+bulk(es, genLoadJson(), chunk_size=1000, request_timeout=30)
+print(datetime.datetime.utcnow())
