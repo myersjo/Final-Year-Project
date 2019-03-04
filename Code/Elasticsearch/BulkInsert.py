7049d7fcb56c0e32979415b1690775a3f8172c17
Jordan Myers <myersjo@tcd.ie>
2019-03-04 11:11:45 +0000

    Updated bulk insert script

--- Code/Elasticsearch/BulkInsert.py
+++ Code/Elasticsearch/BulkInsert.py
@@ -5,6 +5,7 @@
 import argparse
 import json
 import time,datetime
+import subprocess
 from elasticsearch import Elasticsearch # install via  "$ sudo pip install elasticsearch"
 from elasticsearch.helpers import bulk
 
@@ -30,15 +31,26 @@ if args.es_index is not None:
 es = Elasticsearch()
 
 def genLoadJson():
+    lines_read=0
     with open(infile,'r') as f:
         for line in f:
+            status = subprocess.check_output(["sudo", "systemctl", "show", "-p", "SubState", "--value", "elasticsearch.service"])
+            if "running" not in status:
+                subprocess.call(["sudo", "systemctl", "start", "elasticsearch.service"])
+                print("[{}] {} lines read. Restarting failed Elasticsearch...".format(datetime.datetime.utcnow(), lines_read))
+                time.sleep(20)
+            if lines_read % 100 == 0:
+                print("[{}] {} lines read".format(datetime.datetime.now(), lines_read))
+                time.sleep(10)
             j_content = json.loads(line)
+            lines_read += 1
+            time.sleep(1)
             yield {
                 "_index": es_index,
                 "_type": "document",
                 "doc": j_content
             }
 
-print(datetime.datetime.utcnow())
-bulk(es, genLoadJson(), chunk_size=10, request_timeout=30, max_chunk_bytes=5000000, max_retries=0, raise_on_error=False, raise_on_exception=False)
-print(datetime.datetime.utcnow())
+print("[{}] Starting...".format(datetime.datetime.utcnow()))
+(successful_actions, errors) = bulk(es, genLoadJson(), chunk_size=50, request_timeout=30, max_chunk_bytes=5000000, max_retries=3, raise_on_error=False, raise_on_exception=False, stats_only=True)
+print("[{}] Finished with {} successful actions ".format(datetime.datetime.now(), successful_actions))
