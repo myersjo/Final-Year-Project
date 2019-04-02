7478c8847d1624a3a1a38a302fa3839188a77e7d
Jordan Myers <myersjo@tcd.ie>
2019-04-02 21:32:25 +0100

    Added kibana console demo queries

--- Misc/demo.sh
+++ Misc/demo.sh
@@ -21,3 +21,82 @@ es viewallindices
 es deleteindex --help
 
 es deleteindex
+
+
+
+
+GET av-records-fresh-geo-ie*/_search
+{
+  "query": {
+    "match_phrase": {
+      "doc.ip": "37.18.144.35"
+    }
+  },
+  "_source": ["doc.company", "doc.ip", "doc.domain", "doc.run_date"],
+  "sort": [
+    {
+      "doc.run_date": {
+        "order": "desc"
+      }
+    }
+  ]
+}
+
+
+GET av-records-fresh-geo-ie*,av-fingerprints-ie*/_search
+{
+  "query": {
+    "match_phrase": {
+      "doc.ip": "89.191.34.134"
+    }
+  },
+  "_source": ["doc.ip", "doc.run_date", "doc.company", "doc.domain", "doc.fprint"],
+  "sort": [
+    {
+      "doc.run_date": {
+        "order": "desc"
+      }
+    }
+  ]
+}
+
+
+GET records-fresh-ie-*/_search
+{
+  "query": { "match_all": {}},
+  "aggs": {
+    "groupby": {
+      "terms": {
+        "field": "doc.p443.data.http.redirect_response_chain.request.tls_handshake.server_certificates.certificate.parsed.subject_key_info.fingerprint_sha256.keyword",
+        "size": 10
+      }
+    }
+  },
+  "_source": "false",
+  "size": 5
+}
+
+
+GET records-fresh-ie-*/_search
+{
+  "query": { "match_all": {}},
+  "aggs": {
+    "groupby-fp": {
+      "terms": {
+        "field": "doc.p443.data.http.response.request.tls_handshake.server_certificates.certificate.parsed.subject_key_info.fingerprint_sha256.keyword",
+        "size": 10
+      },
+      "aggs": {
+        "groupby-date": {
+          "date_histogram": {
+            "field": "doc.run_date",
+            "interval": "30d",
+            "min_doc_count": 1
+          }
+        }
+      }
+    }
+  },
+  "_source": "false",
+  "size": 5
+}
