3f737920b7ce08fd0d5003b9d7d25f721c16c356
Jordan Myers <myersjo@tcd.ie>
2019-04-02 21:52:59 +0100

    Added more sample queries and comments. Added slides

--- Misc/demo.sh
+++ Misc/demo.sh
@@ -24,7 +24,7 @@ es deleteindex
 
 
 
-
+# Search for IP 37.18.144.35 (Ryanair), sort by run_date newest->oldest
 GET av-records-fresh-geo-ie*/_search
 {
   "query": {
@@ -42,7 +42,7 @@ GET av-records-fresh-geo-ie*/_search
   ]
 }
 
-
+# Search 2 indices for ip 89.191.34.134 (Dublin Aerospace)
 GET av-records-fresh-geo-ie*,av-fingerprints-ie*/_search
 {
   "query": {
@@ -60,7 +60,7 @@ GET av-records-fresh-geo-ie*,av-fingerprints-ie*/_search
   ]
 }
 
-
+# Groups By P443 Sha256 Fingerprint
 GET records-fresh-ie-*/_search
 {
   "query": { "match_all": {}},
@@ -76,7 +76,7 @@ GET records-fresh-ie-*/_search
   "size": 5
 }
 
-
+# Groups By P443 Sha256 Fingerprint, Date
 GET records-fresh-ie-*/_search
 {
   "query": { "match_all": {}},
@@ -99,4 +99,45 @@ GET records-fresh-ie-*/_search
   },
   "_source": "false",
   "size": 5
+}
+
+# Count the number of unique IPs
+GET av-records-fresh-geo-ie*/_search
+{
+  "query": { "match_all": {}},
+  "_source": "false",
+  "size": 0, 
+  "aggs": {
+    "count": {
+      "cardinality": {
+        "field": "doc.ip"
+      }
+    }
+  }
+}
+
+# Count the number of unique IPs by run
+GET av-records-fresh-geo-ie*/_search
+{
+  "query": {
+    "match_all": {}
+  },
+  "_source": "false",
+  "size": 0,
+  "aggs": {
+    "groupby-date": {
+      "date_histogram": {
+        "field": "doc.run_date",
+        "interval": "30d",
+        "min_doc_count": 1
+      },
+      "aggs": {
+        "count": {
+          "cardinality": {
+            "field": "doc.ip"
+          }
+        }
+      }
+    }
+  }
 }
